650966ce3146c6b028c8f2b212967f87079c54ff
ilaoniu <tony@tonydeMacBook.local>
2015-09-15 09:51:29 +0800

    Initial Commit

---
+++ NiuScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NiuScrollView
+//
+//  Created by ilaoniu on 15-9-15.
+//  Copyright (c) 2015年 ilaoniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NiuScrollView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NiuScrollView
+//
+//  Created by ilaoniu on 15-9-15.
+//  Copyright (c) 2015年 ilaoniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
